40b37a85567faba18feb388366f081e103c1a67e
samarth <samarth.magdallawala@krty.com>
2017-04-20 11:16:30 +0530

    Dynamic Forms based on json data

--- src/EasyHRMS_Angular/angularApp/app/layout/lookup/branch/form/form.component.ts
+++ src/EasyHRMS_Angular/angularApp/app/layout/lookup/branch/form/form.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, Validators, FormControl} from '@angular/forms';
+import { QuestionModels } from '../../../../models/question-models';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit {
-
-  formbuilder: any[];
-
-  constructor() { }
-
+  cols: any[];
+  form: FormGroup;
+  payLoad = '';
+  constructor() {
+     this.cols = [
+            {key: 'lastName', text: 'Last name', required: true, order: 2, 'controlType': 'textbox'},
+            {key: 'firstName', text: 'First name', required: true, order: 1, 'controlType': 'textbox'},
+            {key: 'emailAddress', text: 'Email', required: false, type: 'email', order: 1, 'controlType': 'textbox'},
+            {key: 'country', text: 'Country',
+            'options': [
+              {key: 'usa', value: 'USA'},
+              {key: 'germany', value: 'Germany'},
+              {key: 'canada', value: 'Canada'},
+              {key: 'australia', value: 'Australia'}
+              ],
+            order: 1, 'controlType': 'dropdown'}
+        ];
+        this.cols.sort ( ( a, b ) => a.order - b.order );
+  }
   ngOnInit() {
-      this.formbuilder = [
-        ""
-      ];
+        this.form = this.toGroup();
     }
+   onSubmit() {
+     this.payLoad = JSON.stringify(this.form.value);
+    }
+    toGroup() {
+        let group: any = {};
 
+        this.cols.forEach((question) => {
+            if (question.required) {
+                group[question.key] = new FormControl('', Validators.required);
+            } else {
+                group[question.key] = new FormControl('');
+            }
+        });
+
+        return new FormGroup(group);
+    }
 }
