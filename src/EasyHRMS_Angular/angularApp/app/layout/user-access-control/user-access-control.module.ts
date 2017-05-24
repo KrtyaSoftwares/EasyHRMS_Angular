a9b9c96a56da892de9801c48d38749c7f43f60e5
samarth <samarth.magdallawala@krty.com>
2017-05-24 10:36:55 +0530

    Permission module

--- src/EasyHRMS_Angular/angularApp/app/layout/user-access-control/user-access-control.module.ts
+++ src/EasyHRMS_Angular/angularApp/app/layout/user-access-control/user-access-control.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserAccessControlRoutingModule } from './user-access-control-routing.module';
 import { UserAccessControlComponent } from './components/user-access-control.component';
+import {InputSwitchModule} from 'primeng/primeng';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserAccessControlRoutingModule
+    UserAccessControlRoutingModule,
+    InputSwitchModule
   ],
   declarations: [
     UserAccessControlComponent
